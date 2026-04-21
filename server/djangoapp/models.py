f910136249d8d388d6e219be288f738f8a29337b
petrusz2002 <petreusz2002@gmail.com>
2026-04-21 06:33:04 -0400

    madjnem

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,10 +1,6 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Car Make model
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
@@ -13,36 +9,40 @@ class CarMake(models.Model):
         return self.name
 
 
-# Car Model model
 class CarModel(models.Model):
 
-    # Type choices
     SEDAN = 'Sedan'
     SUV = 'SUV'
     WAGON = 'Wagon'
+    HATCHBACK = 'Hatchback'
+    COUPE = 'Coupe'
+    CONVERTIBLE = 'Convertible'
+    PICKUP = 'Pickup'
+    MINIVAN = 'Minivan'
 
     CAR_TYPES = [
         (SEDAN, 'Sedan'),
         (SUV, 'SUV'),
         (WAGON, 'Wagon'),
+        (HATCHBACK, 'Hatchback'),
+        (COUPE, 'Coupe'),
+        (CONVERTIBLE, 'Convertible'),
+        (PICKUP, 'Pickup'),
+        (MINIVAN, 'Minivan'),
     ]
 
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
 
-    dealer_id = models.IntegerField()
+    dealer_id = models.IntegerField(null=True, blank=True)
 
     name = models.CharField(max_length=100)
 
-    type = models.CharField(
-        max_length=10,
-        choices=CAR_TYPES,
-        default=SEDAN
-    )
+    type = models.CharField(max_length=20, choices=CAR_TYPES, default=SUV)
 
     year = models.IntegerField(
         validators=[
             MinValueValidator(2015),
-            MaxValueValidator(2023)
+            MaxValueValidator(2026)
         ]
     )
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -81,8 +81,8 @@ def registration(request):
 def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
-    if(count == 0):
-        initiate()
+    ##if(count == 0):
+    initiate()
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
